aed9d7a7d07e278e31419801a8e17bd6167ac27a
Joel Madsen <joel.madsen10@gmail.com>
2021-03-03 14:48:12 +0100

    Seperated view and controller into seperate classes

--- Puslespil_Eclipse/Puslespil/src/controller/Main.java
+++ Puslespil_Eclipse/Puslespil/src/controller/Main.java
@@ -2,27 +2,30 @@ package controller;
 
 import model.Piece;
 import processing.core.PApplet;
+import view.View;
 
-public class Main extends PApplet{
-	Piece p;
+public class Main{
+	// The argument passed to main must match the class name
+	public static void main(String[] args) {
+		//View view = new View();
+		// Set the main
+		//PApplet.main("view.View");
+		
+		View view = new View();
+		PApplet.runSketch(new String[]{"--location=200,200", ""}, view);
+		
+		
+		Piece p = new Piece(view, 200, 200, 100, 100);
+		Piece p1 = new Piece(view, 100, 100, 100, 100);
+		Piece p2 = new Piece(view, 100, 300, 100, 100);
+		view.addPieceToList(p);
+		view.addPieceToList(p1);
+		view.addPieceToList(p2);
+		
+		System.out.println();
+		
+		//view.addPieceToList(p);
+		
+	}
 	
-	 // The argument passed to main must match the class name
-   public static void main(String[] args) {
-       PApplet.main("controller.Main");
-   }
-   
-   // method used only for setting the size of the window
-   public void settings(){
-   	size(400,400);
-   }
-
-   // identical use to setup in Processing IDE except for size()
-   public void setup(){
-       p = new Piece(this, 200, 200, 100, 100);
-   }
-
-   // identical use to draw in Processing IDE
-   public void draw(){
-   	p.display();
-   }
 }
--- Puslespil_Eclipse/Puslespil/src/view/View.java
+++ Puslespil_Eclipse/Puslespil/src/view/View.java
@@ -1,19 +1,41 @@
 package view;
 
+import java.util.ArrayList;
+
 import model.Piece;
 import processing.core.PApplet;
 
-public class View {
-	// Variables
-	PApplet Main;
+public class View extends PApplet {
 	
-	// Constructor
-	public View(PApplet _Main) {
+	public View() {
+		System.out.println("hi");
+	}
+	
+	// Variables
+	ArrayList<Piece> pieceList = new ArrayList<Piece>();
+	
+	// identical use to setup in Processing IDE except for size()
+	public void setup() {
 		
 	}
 	
-	// Functions
-	public void initView(int screenWidth, int screenHeight) {
-		Main.size(screenWidth, screenHeight);
+	// method used only for setting the size of the window
+	public void settings() {
+		// Create the view object
+		size(800, 800);
+	}
+	
+	// identical use to draw in Processing IDE
+	public void draw(){
+		// Draw the pieces
+		if(pieceList.size() != 0) {
+			for(Piece piece : pieceList) {
+				piece.display();
+			}
+		}
+	}
+	
+	public void addPieceToList(Piece piece) {
+		pieceList.add(piece);
 	}
 }
