1ba9938e154976f537286ac8b31aff7ac7700706
Joel Madsen <joel.madsen10@gmail.com>
2021-03-03 13:43:51 +0100

    Changed main class

--- Puslespil_Eclipse/Puslespil/src/controller/Main.java
+++ Puslespil_Eclipse/Puslespil/src/controller/Main.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class Main {
+import model.Piece;
+import processing.core.PApplet;
 
+public class Main extends PApplet{
+	Piece p;
+	
+	 // The argument passed to main must match the class name
+   public static void main(String[] args) {
+       PApplet.main("controller.Main");
+   }
+   
+   // method used only for setting the size of the window
+   public void settings(){
+   	size(400,400);
+   }
+
+   // identical use to setup in Processing IDE except for size()
+   public void setup(){
+       p = new Piece(this, 200, 200, 100, 100);
+   }
+
+   // identical use to draw in Processing IDE
+   public void draw(){
+   	p.display();
+   }
 }
--- Puslespil_Eclipse/Puslespil/src/view/View.java
+++ Puslespil_Eclipse/Puslespil/src/view/View.java
@@ -3,26 +3,6 @@ package view;
 import model.Piece;
 import processing.core.PApplet;
 
-public class View extends PApplet {
-	Piece p;
+public class View {
 	
-	 // The argument passed to main must match the class name
-    public static void main(String[] args) {
-        PApplet.main("view.View");
-    }
-    
-    // method used only for setting the size of the window
-    public void settings(){
-    	size(400,400);
-    }
-
-    // identical use to setup in Processing IDE except for size()
-    public void setup(){
-        p = new Piece(this, 200, 200, 100, 100);
-    }
-
-    // identical use to draw in Processing IDE
-    public void draw(){
-    	p.display();
-    }
 }
