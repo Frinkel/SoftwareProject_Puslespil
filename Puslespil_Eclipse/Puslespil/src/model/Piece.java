acb38357d085a5a1f230baa8b028cc75dd4dd794
Joel Madsen <joel.madsen10@gmail.com>
2021-03-03 13:42:11 +0100

    Imported processing

--- Puslespil_Eclipse/Puslespil/src/model/Piece.java
+++ Puslespil_Eclipse/Puslespil/src/model/Piece.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Piece {
+import processing.core.PApplet;
 
+public class Piece {
+	
+	PApplet p5;
+	
+	float center_x, center_y, r, r1;
+	public Piece(PApplet _p5, float cx, float cy, float r, float r1){
+		this.center_x = cx;
+		this.center_y = cy;
+		this.r = r;
+		this.r1 = r1;
+		
+		this.p5 = _p5;
+	}
+	
+	public void display() {
+		
+		p5.ellipse(center_x, center_y, r, r1);
+	}
 }
