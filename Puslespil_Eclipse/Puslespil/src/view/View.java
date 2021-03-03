41bbfe240d514665a9254a2a819d8030d2f9a741
Joel Madsen <joel.madsen10@gmail.com>
2021-03-03 13:59:52 +0100

    Java SDK change

--- Puslespil_Eclipse/Puslespil/src/view/View.java
+++ Puslespil_Eclipse/Puslespil/src/view/View.java
@@ -4,5 +4,15 @@ import model.Piece;
 import processing.core.PApplet;
 
 public class View {
+	// Variables
+	int screenWidth, screenHeight;
+	
+	// Constructor
+	public View(int scwidth, int scheight) {
+		this.screenWidth = scwidth;
+		this.screenHeight = scheight;
+	}
+	
+	// Functions
 	
 }
