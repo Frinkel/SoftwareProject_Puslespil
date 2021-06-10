20bb405125f0dbffbefd262488e2c993a5edc369
Moosecj <carlj0908@gmail.com>
2021-06-10 14:46:26 +0200

    merge of puzzlesolver

--- Puslespil_Eclipse/Puslespil/src/controller/PuzzleSolver.java
+++ Puslespil_Eclipse/Puslespil/src/controller/PuzzleSolver.java
@@ -29,19 +29,31 @@ public class PuzzleSolver {
 		}
 		Object[] groups = groupPieces(pieceList);
 
-		Object[] adjacencyAndMatchingArray = produceAngleLengthPairs(AncleLengthPieceList, (ArrayList<Integer>) groups[0], (ArrayList<Integer>) groups[1],
-				(ArrayList<Integer>) groups[2], (int) groups[3], (int) groups[4], (int) groups[5]);
+		Object[] adjacencyAndMatchingArray = produceAngleLengthPairs(AncleLengthPieceList,
+				(ArrayList<Integer>) groups[0], (ArrayList<Integer>) groups[1], (ArrayList<Integer>) groups[2],
+				(int) groups[3], (int) groups[4], (int) groups[5]);
 		Object[] adjacencyArray = (Object[]) adjacencyAndMatchingArray[0];
 		Object[] matchingArray = (Object[]) adjacencyAndMatchingArray[1];
-		
+
+		for (int i = 0; i < adjacencyArray.length; i++) {
+			int[] alreadyCovered = new int[matchingArray.length];
+			int[] adjacencyPair = (int[]) adjacencyArray[i];
+			for(int j = 1; j < adjacencyPair.length;j++) {
+				alreadyCovered[i] = adjacencyPair[0];
+				
+			}
+			// code
+		}
+
 		Point2D.Float[][] threeMatchingPoints = getPointsOfTwoSuitablePieces((int[]) matchingArray[0], pieceList);
 		System.out.println(Arrays.deepToString(threeMatchingPoints));
-		
-		
-		System.out.println(pC.getAngleFromThreePoints(threeMatchingPoints[0][0], threeMatchingPoints[0][1], threeMatchingPoints[0][2]));
+
+		System.out.println(pC.getAngleFromThreePoints(threeMatchingPoints[0][0], threeMatchingPoints[0][1],
+				threeMatchingPoints[0][2]));
 		System.out.println("equals? ");
-		System.out.println(pC.getAngleFromThreePoints(threeMatchingPoints[1][0], threeMatchingPoints[1][1], threeMatchingPoints[1][2]));
-		
+		System.out.println(pC.getAngleFromThreePoints(threeMatchingPoints[1][0], threeMatchingPoints[1][1],
+				threeMatchingPoints[1][2]));
+
 	}
 
 	public Object[] groupPieces(Object[] pieceList) {
@@ -112,9 +124,9 @@ public class PuzzleSolver {
 		return new Object[] { gr1, gr2, gr3, gr1Length, gr2Length, gr3Length };
 	}
 
-	private Object[] produceAngleLengthPairs(Object[] AncleLengthPieceList, ArrayList<Integer> gr1, ArrayList<Integer> gr2,
-			ArrayList<Integer> gr3, int gr1Length, int gr2Length, int gr3Length) {
-		// TODO Auto-generated method stub
+	private Object[] produceAngleLengthPairs(Object[] AncleLengthPieceList, ArrayList<Integer> gr1,
+		ArrayList<Integer> gr2, ArrayList<Integer> gr3, int gr1Length, int gr2Length, int gr3Length) {
+
 		Object[] adjacencyArray = new Object[gr1.size() + gr2.size() + gr3.size()];
 		Object[] matchingArray = new Object[2 * gr1.size() + 3 * gr2.size() + 4 * gr3.size()];
 		int k = 0;
@@ -125,14 +137,21 @@ public class PuzzleSolver {
 				System.out.println(" is in first case");
 				for (int j = 0; j < gr1.size(); j++) {
 					if (i != j) {
-						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
-								(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr1.get(i), gr1.get(j));
-						int neighbourInt = toAddMatching[1];
-						if (neighbourInt != -1) {
-							toAdd.add(neighbourInt);
-							matchingArray[k] = toAddMatching;
-							k++;
+						if (!containsPair(matchingArray, gr1.get(i), gr1.get(j))) {
+							int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
+									(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr1.get(i), gr1.get(j));
+							int neighbourInt = toAddMatching[1];
+							if (neighbourInt != -1) {
+								toAdd.add(neighbourInt);
+								matchingArray[k] = toAddMatching;
+								k++;
+							} else {
+
+							}
+						}else {
+							toAdd.add(gr1.get(j));
 						}
+
 					}
 				}
 				System.out.println(toAdd.toString());
@@ -141,27 +160,34 @@ public class PuzzleSolver {
 			} else if (gr3.isEmpty()) {
 				for (int j = 0; j < gr1.size(); j++) {
 					if (i != j) {
+						if (!containsPair(matchingArray, gr1.get(i), gr1.get(j))) {
+							int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
+									(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr1.get(i), gr1.get(j));
+							int neighbourInt = toAddMatching[1];
+							if (neighbourInt != -1) {
+								toAdd.add(neighbourInt);
+								matchingArray[k] = toAddMatching;
+								k++;
+							}
+						}else {
+							toAdd.add(gr1.get(j));
+						}
+					}
+				}
+
+				for (int j = 0; j < gr2.size(); j++) {
+					if (!containsPair(matchingArray, gr1.get(i), gr2.get(j))) {
 						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
-								(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr1.get(i), gr1.get(j));
+								(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr1.get(i), gr2.get(j));
 						int neighbourInt = toAddMatching[1];
 						if (neighbourInt != -1) {
 							toAdd.add(neighbourInt);
 							matchingArray[k] = toAddMatching;
 							k++;
 						}
+					}else {
+						toAdd.add(gr2.get(j));
 					}
-				}
-
-				for (int j = 0; j < gr2.size(); j++) {
-					int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
-							(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr1.get(i), gr2.get(j));
-					int neighbourInt = toAddMatching[1];
-					if (neighbourInt != -1) {
-						toAdd.add(neighbourInt);
-						matchingArray[k] = toAddMatching;
-						k++;
-					}
-
 				}
 				System.out.println(toAdd.toString());
 
@@ -169,13 +195,17 @@ public class PuzzleSolver {
 			} else {
 				System.out.println("is in last case");
 				for (int j = 0; j < gr2.size(); j++) {
-					int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
-							(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr1.get(i), gr2.get(j));
-					int neighbourInt = toAddMatching[1];
-					if (neighbourInt != -1) {
-						toAdd.add(neighbourInt);
-						matchingArray[k] = toAddMatching;
-						k++;
+					if (!containsPair(matchingArray, gr1.get(i), gr2.get(j))) {
+						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr1.get(i)],
+								(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr1.get(i), gr2.get(j));
+						int neighbourInt = toAddMatching[1];
+						if (neighbourInt != -1) {
+							toAdd.add(neighbourInt);
+							matchingArray[k] = toAddMatching;
+							k++;
+						}
+					}else {
+						toAdd.add(gr2.get(j));
 					}
 
 				}
@@ -190,26 +220,34 @@ public class PuzzleSolver {
 			toAdd.add(gr2.get(i));
 			if (gr3.isEmpty()) {
 				for (int j = 0; j < gr1.size(); j++) {
-					int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
-							(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr2.get(i), gr1.get(j));
-					int neighbourInt = toAddMatching[1];
-
-					if (neighbourInt != -1) {
-						toAdd.add(neighbourInt);
-						matchingArray[k] = toAddMatching;
-						k++;
-					}
-				}
-				for (int j = 0; j < gr2.size(); j++) {
-					if (i != j) {
+					if (!containsPair(matchingArray, gr2.get(i), gr1.get(j))) {
 						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
-								(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr2.get(i), gr2.get(j));
+								(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr2.get(i), gr1.get(j));
 						int neighbourInt = toAddMatching[1];
-
+	
 						if (neighbourInt != -1) {
 							toAdd.add(neighbourInt);
 							matchingArray[k] = toAddMatching;
 							k++;
+						}
+					}else {
+						toAdd.add(gr1.get(j));
+					}
+				}
+				for (int j = 0; j < gr2.size(); j++) {
+					if (i != j) {
+						if (!containsPair(matchingArray, gr2.get(i), gr2.get(j))) {
+							int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
+									(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr2.get(i), gr2.get(j));
+							int neighbourInt = toAddMatching[1];
+	
+							if (neighbourInt != -1) {
+								toAdd.add(neighbourInt);
+								matchingArray[k] = toAddMatching;
+								k++;
+							}
+						}else {
+							toAdd.add(gr2.get(j));
 						}
 					}
 				}
@@ -218,39 +256,51 @@ public class PuzzleSolver {
 				adjacencyArray[i + gr1.size()] = toAdd;
 			} else {
 				for (int j = 0; j < gr1.size(); j++) {
-					int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
-							(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr2.get(i), gr1.get(j));
-					int neighbourInt = toAddMatching[1];
-
-					if (neighbourInt != -1) {
-						toAdd.add(neighbourInt);
-						matchingArray[k] = toAddMatching;
-						k++;
-					}
-				}
-				for (int j = 0; j < gr2.size(); j++) {
-					if (i != j) {
+					if (!containsPair(matchingArray, gr2.get(i), gr1.get(j))) {
 						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
-								(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr2.get(i), gr2.get(j));
+								(AncleLength[]) AncleLengthPieceList[gr1.get(j)], gr2.get(i), gr1.get(j));
 						int neighbourInt = toAddMatching[1];
-
+	
 						if (neighbourInt != -1) {
 							toAdd.add(neighbourInt);
 							matchingArray[k] = toAddMatching;
 							k++;
 						}
+					}else {
+						toAdd.add(gr1.get(j));
+					}
+				}
+				for (int j = 0; j < gr2.size(); j++) {
+					if (i != j) {
+						if (!containsPair(matchingArray, gr2.get(i), gr2.get(j))) {
+							int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
+									(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr2.get(i), gr2.get(j));
+							int neighbourInt = toAddMatching[1];
+	
+							if (neighbourInt != -1) {
+								toAdd.add(neighbourInt);
+								matchingArray[k] = toAddMatching;
+								k++;
+							}
+						}else {
+							toAdd.add(gr2.get(j));
+						}
 					}
 				}
 
 				for (int j = 0; j < gr3.size(); j++) {
-					int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
-							(AncleLength[]) AncleLengthPieceList[gr3.get(j)], gr2.get(i), gr3.get(j));
-					int neighbourInt = toAddMatching[1];
-
-					if (neighbourInt != -1) {
-						toAdd.add(neighbourInt);
-						matchingArray[k] = toAddMatching;
-						k++;
+					if (!containsPair(matchingArray, gr2.get(i), gr3.get(j))) {
+						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr2.get(i)],
+								(AncleLength[]) AncleLengthPieceList[gr3.get(j)], gr2.get(i), gr3.get(j));
+						int neighbourInt = toAddMatching[1];
+	
+						if (neighbourInt != -1) {
+							toAdd.add(neighbourInt);
+							matchingArray[k] = toAddMatching;
+							k++;
+						}
+					}else {
+						toAdd.add(gr3.get(j));
 					}
 				}
 				System.out.println(toAdd.toString());
@@ -264,26 +314,34 @@ public class PuzzleSolver {
 			ArrayList<Integer> toAdd = new ArrayList<Integer>();
 			toAdd.add(gr3.get(i));
 			for (int j = 0; j < gr2.size(); j++) {
-				int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr3.get(i)],
-						(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr3.get(i), gr2.get(j));
-				int neighbourInt = toAddMatching[1];
-
-				if (neighbourInt != -1) {
-					toAdd.add(neighbourInt);
-					matchingArray[k] = toAddMatching;
-					k++;
-				}
-			}
-			for (int j = 0; j < gr3.size(); j++) {
-				if (i != j) {
+				if (!containsPair(matchingArray, gr3.get(i), gr2.get(j))) {
 					int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr3.get(i)],
-							(AncleLength[]) AncleLengthPieceList[gr3.get(j)], gr3.get(i), gr3.get(j));
+							(AncleLength[]) AncleLengthPieceList[gr2.get(j)], gr3.get(i), gr2.get(j));
 					int neighbourInt = toAddMatching[1];
-
+	
 					if (neighbourInt != -1) {
 						toAdd.add(neighbourInt);
 						matchingArray[k] = toAddMatching;
 						k++;
+					}else {
+						toAdd.add(gr2.get(j));
+					}
+				}
+			}
+			for (int j = 0; j < gr3.size(); j++) {
+				if (i != j) {
+					if (!containsPair(matchingArray, gr3.get(i), gr3.get(j))) {
+						int[] toAddMatching = checkTwoPieces((AncleLength[]) AncleLengthPieceList[gr3.get(i)],
+								(AncleLength[]) AncleLengthPieceList[gr3.get(j)], gr3.get(i), gr3.get(j));
+						int neighbourInt = toAddMatching[1];
+	
+						if (neighbourInt != -1) {
+							toAdd.add(neighbourInt);
+							matchingArray[k] = toAddMatching;
+							k++;
+						}
+					}else {
+						toAdd.add(gr3.get(j));
 					}
 				}
 			}
@@ -294,11 +352,24 @@ public class PuzzleSolver {
 		}
 		System.out.println("adjacency : " + Arrays.toString(adjacencyArray));
 		System.out.println("matchingarray : " + Arrays.deepToString(matchingArray));
-		return new Object[] {adjacencyArray, matchingArray};
-		
-		
-		
+		return new Object[] { adjacencyArray, matchingArray };
 
+	}
+
+	private boolean containsPair(Object[] matchingArray, int integer, int integer2) {
+		
+		for (int i = 0; i < matchingArray.length; i++) {
+			
+			if(matchingArray[i] == (null)) {
+				System.out.println("hello");
+				return false;
+			}
+			int[] pairToCheck = (int[]) matchingArray[i];
+			if ((pairToCheck[0] == integer2 && pairToCheck[1] == integer) || (pairToCheck[1] == integer2 && pairToCheck[0] == integer)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private int[] checkTwoPieces(AncleLength[] angleLength1, AncleLength[] angleLength2, int index0, int index1) {
@@ -321,15 +392,14 @@ public class PuzzleSolver {
 		}
 		return new int[] { -1, -1, -1, -1 };
 	}
-	
+
 	private Float[][] getPointsOfTwoSuitablePieces(int[] matchingPoints, Object[] pieceList) {
 		Point2D.Float[][] threeMatchingPoints = new Point2D.Float[2][3];
-				
-		
-		for(int i = 0; i < 2;i++) {
+
+		for (int i = 0; i < 2; i++) {
 			Point2D.Float[] piece = (Float[]) pieceList[matchingPoints[i]];
-			for(int j = 0; j < 3;j++) {
-				threeMatchingPoints[i][j] = piece[(matchingPoints[i+2]+j)%(piece.length)];
+			for (int j = 0; j < 3; j++) {
+				threeMatchingPoints[i][j] = piece[(matchingPoints[i + 2] + j) % (piece.length)];
 			}
 		}
 		return threeMatchingPoints;
